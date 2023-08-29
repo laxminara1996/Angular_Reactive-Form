7a98f034404164fd7ff1535bad04441140c889a0
laxminara1996 <laxminara1996@gmail.com>
2023-08-29 19:47:07 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  form:FormGroup;
+  constructor(){
+    this.form =  new FormGroup({
+      username:new FormControl("",[Validators.required,Validators.pattern("^[a-zA-Z0-9']*"),Validators.maxLength(3)]),
+      password:new FormControl("",[Validators.required,Validators.pattern("^[a-zA-Z0-9']*"),Validators.maxLength(6)])
+
+    })
+  }
+  get u(){
+    return this.form.controls;
+  }
+  get p(){
+    return this.form.controls;
+  }
+  login():void{
+    console.log(this.form.value);
+  }
 }
